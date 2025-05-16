4a0d49a3a819df7fd286e51bed68900fd8fa0cef
Stephen Cole <colestephen@hotmail.com>
2025-05-16 19:16:24 -0300

    MAY16

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+// pages/_app.js or _app.tsx
+import Head from 'next/head';
 import '../styles/globals.css';
 import '../styles/fonts.css';
 import { Inter } from 'next/font/google';
@@ -5,14 +7,17 @@ import Layout from '../components/Layout';
 
 const inter = Inter({ subsets: ['latin'] });
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
-
 export default function MyApp({ Component, pageProps }) {
   return (
-    <main className={inter.className}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </main>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
+      </Head>
+      <main className={inter.className}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </main>
+    </>
   );
 }
