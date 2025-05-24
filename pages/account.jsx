fa270b221cddfbeb39e92cd2e725f34284863bcf
Stephen Cole <colestephen@hotmail.com>
2025-05-24 05:40:07 -0300

    MAY24

--- pages/account.jsx
+++ pages/account.jsx
@@ -420,10 +420,6 @@ export default function Account() {
       <hr className="my-6" />
       <SupportTicketForm />
 
-      <Link href="/admin/support/threads" className="text-blue-600 underline">
-        iew Support Threads
-      </Link>
-
       {showPauseConfirm && (
         <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-40">
           <div className="bg-white rounded-lg p-6 w-full max-w-md shadow-lg">
