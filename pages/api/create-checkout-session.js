57c61bc6bd45402844f1af73359db9df59d9cc7b
Stephen Cole <colestephen@hotmail.com>
2025-06-17 08:40:09 -0300

    Update Success URL

--- pages/api/create-checkout-session.js
+++ pages/api/create-checkout-session.js
@@ -71,8 +71,8 @@ export default async function handler(req, res) {
         trial_period_days: 23
       },
       
-      success_url: `http://localhost:3000/dashboard?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `http://localhost:3000/signup`,
+      success_url: `${req.headers.origin}/dashboard?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${req.headers.origin}/signup`,
     });
 
     console.log('✅ Stripe session created:', session.url);
